0cb17fdb1b62502348bfba91045052b3d3846451
Raghavendra Reddy <raghavendrareddy@salesforce.com>
2020-08-24 20:27:26 +0530

    Add antd css to global file

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { DatePicker } from 'antd';
-import 'antd/dist/antd.css';
 
 export default function Home() {
   return (
